5b702fdb0e8f59651d826496fbc3e8714f81a62e
안도훈 <ahndohun1@gmail.com>
2019-02-13 23:47:29 +0900

    router-link 태그를 이용해 navigation 컴포넌트 추가

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
+    <Navbar/>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from "./components/NavBar.vue";
+
 export default {
   name: "app",
+  components: {
+    Navbar
+  },
   data() {
     return {
       msg: "Welcome to Your Vue.js App"
