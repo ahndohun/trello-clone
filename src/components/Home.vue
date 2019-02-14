70704da049f8d79dfb2a6752218c0794821949c3
안도훈 <ahndohun1@gmail.com>
2019-02-14 22:01:40 +0900

    XMLHttpRequest를 이용해 api 통신 기능 구현

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,52 @@
 <template>
   <div>home
     <div>board list:
-      <ul>
-        <li>
-          <router-link to="/board/1">board1</router-link>
-        </li>
-        <li>
-          <router-link to="/board/2">board2</router-link>
-        </li>
-      </ul>
+      <div v-if="loading">로딩중</div>
+      <div v-else>
+        {{ apiRes }}
+        <ul>
+          <li>
+            <router-link to="/board/1">board1</router-link>
+          </li>
+          <li>
+            <router-link to="/board/2">board2</router-link>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      loading: false,
+      apiRes: ""
+    };
+  },
+  methods: {
+    fetchData() {
+      this.loading = true;
+
+      const req = new XMLHttpRequest();
+
+      req.open("GET", "http://localhost:3000/health");
+
+      req.send();
+
+      req.addEventListener("load", () => {
+        this.loading = false;
+        this.apiRes = {
+          status: req.status,
+          statusText: req.statusText,
+          response: JSON.parse(req.response)
+        };
+      });
+    }
+  },
+  created() {
+    this.fetchData();
+  }
+};
 </script>
