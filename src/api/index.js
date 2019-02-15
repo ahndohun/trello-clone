2b14336ee75911daf67b58a8a3150aff1aa41213
안도훈 <ahndohun1@gmail.com>
2019-02-15 23:48:28 +0900

    로그인 인증 통신 부분 구현

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@ import router from "../router";
 const DOMAIN = "http://localhost:3000";
 const UNAUTHORIZED = 401;
 const onUnauthororized = () => {
-  router.push("/login");
+  router.push(`/login?rPath=${encodeURIComponent(location.pathname)}`);
 };
 
 const request = (methods, url, data) => {
@@ -16,13 +16,19 @@ const request = (methods, url, data) => {
     .then(result => result.data)
     .catch(result => {
       const { status } = result.response;
-      if (status === UNAUTHORIZED) return onUnauthororized();
-      throw Error(result);
+      if (status === UNAUTHORIZED) onUnauthororized();
+      throw result.response;
     });
 };
 
 export const board = {
   fetch() {
     return request("get", "/boards");
+  }
+};
+
+export const auth = {
+  login(email, password) {
+    return request("post", "/login", { email, password });
   }
 };
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,41 @@
 <template>
-  <div>login</div>
+  <div>login
+    <form @submit.prevent="onSubmit">
+      <input type="email" v-model="email">
+      <input type="password" v-model="password">
+      <button :disabled="invaildForm">로그인</button>
+      <p v-if="error">{{ error }}</p>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {};
+import { auth } from "../api";
+
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      error: ""
+    };
+  },
+  computed: {
+    invaildForm() {
+      return !this.email || !this.password;
+    }
+  },
+  methods: {
+    onSubmit() {
+      auth
+        .login(this.email, this.password)
+        .then(data => {
+          console.log(data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
+};
 </script>
